c5df7cfbb94577e3e42c2cc1779f5a8605d92dea
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-02-29 14:20:55 +0100

    Debuging packages exports

--- src/logger/LoggerClient.ts
+++ src/logger/LoggerClient.ts
@@ -4,23 +4,25 @@ import { LoggingService } from "./LoggingService";
 import { LogLevel } from "../types/LogLevel";
 import ServiceMetadata from "../types/ServiceMetadata";
 import { ColorChoice } from "../types/TerminalColors";
+import { LoggerMode } from "../types/LoggerMode";
 
-abstract class LoggerClient {
-    protected static loggerConfig?: LoggerConfig;
-    protected static loggerStylesConfig?: LoggerStylesConfig;
+// Configuration par défaut pour LoggerStylesConfig
+const defaultLoggerStylesConfig = new LoggerStylesConfig();
 
-    // Définir la configuration pour LoggerClient
-    public static configure(loggerConfig: LoggerConfig, loggerStylesConfig: LoggerStylesConfig): void {
-        LoggerClient.loggerConfig = loggerConfig;
-        LoggerClient.loggerStylesConfig = loggerStylesConfig;
-    }
-
+export abstract class LoggerClient {
     protected serviceMetadata: ServiceMetadata;
 
-    constructor(serviceName: string, isMainProcess: boolean, color?: ColorChoice) {
-        this.ensureLoggingServiceInitialized();
-        this.serviceMetadata = { serviceName, isMainProcess, color };
+    constructor(serviceName: string, isMainProcess: boolean = false, logLevel: LogLevel = LogLevel.INFO, color?: ColorChoice) {
+        // Crée une configuration de logger spécifique à ce client
+        const loggerConfig = new LoggerConfig(logLevel, LoggerMode.CLASSIC);
+        
+        // Initialise LoggingService avec la configuration spécifique si nécessaire
+        this.ensureLoggingServiceInitialized(loggerConfig);
 
+        // Configuration spécifique du service client
+        this.serviceMetadata = { serviceName, isMainProcess, color };
+        
+        // Enregistrement du service
         LoggingService.getInstance().registerServiceIfNeeded(this.serviceMetadata);
     }
 
@@ -28,10 +30,13 @@ abstract class LoggerClient {
         LoggingService.getInstance().log(this.serviceMetadata, level, message);
     }
 
-    private ensureLoggingServiceInitialized(): void {
-        // Vérifie si LoggingService a déjà été initialisé
-        if (!LoggingService.isInitialized() && LoggerClient.loggerConfig && LoggerClient.loggerStylesConfig) {
-            LoggingService.initialize(LoggerClient.loggerConfig, LoggerClient.loggerStylesConfig);
+    private ensureLoggingServiceInitialized(loggerConfig: LoggerConfig): void {
+        if (!LoggingService.isInitialized()) {
+            // Initialise le service de log avec la configuration spécifique au client et la configuration par défaut des styles
+            LoggingService.initialize(loggerConfig, defaultLoggerStylesConfig);
+        } else {
+            // Optionnel : Mettre à jour la configuration si nécessaire
+            // LoggingService.getInstance().updateConfig(loggerConfig, defaultLoggerStylesConfig);
         }
     }
 }
