d81a0f2ac99ac96cb8510c1d8a4c3d0c8ace66fd
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-04 13:45:22 +0100

    Quick fix in Logger Client (log function params)

--- src/logger/clients/LoggerClient.ts
+++ src/logger/clients/LoggerClient.ts
@@ -9,12 +9,12 @@ export abstract class LoggerClient {
         this.serviceName = serviceName;
     }
 
-    public log(level: LogLevel, message: string): void {
+    public log(level: LogLevel, ...messages: any[]): void {
         if(!this.processId) {
             throw new Error("Process ID is not set. Please init the client before logging messages.");
         }
         // Utilisation du service de journalisation pour enregistrer le message
         // avec l'ID du processus pour identifier le service source
-        LoggingService.getInstance().log(this.processId, level, message);
+        LoggingService.getInstance().log(this.processId, level, messages);
     }
 }
